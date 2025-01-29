9b0b01b5a8a20eccb35111058c6ed6514e27247b
JayeshW7 <waghjayesh07@gmail.com>
2025-01-29 13:03:31 +0530

    Meditation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:ableeasefinale/pages/RegisterPatientDoctor.dart';
-import 'package:ableeasefinale/pages/SignInPatientDoctor.dart';
 import 'package:ableeasefinale/pages/UI/loginPage.dart';
 import 'package:ableeasefinale/pages/UI/signIn.dart';
-import 'package:ableeasefinale/pages/doctorPages/doctorSignIn.dart';
 import 'package:ableeasefinale/providers/user_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -42,10 +39,7 @@ class MyApp extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.active) {
               if (snapshot.hasData) {
-                // return const ParentPage();
-                return const LoginPage();
-                // return const SigninPatientdoctor();
-                // return const DoctorSignIn();
+                return const ParentPage();
               } else if (snapshot.hasError) {
                 return Center(
                   child: Text('${snapshot.error}'),
@@ -56,9 +50,7 @@ class MyApp extends StatelessWidget {
               return const Center(child: CircularProgressIndicator());
             }
 
-            // return const LoginPage();
-            return const RegPatientdoctor();
-            // return const DoctorSignIn();
+            return const LoginPage();
           },
         ),
         // home: MedTest(),
